369dbbc8d09f3da31a498fc22010ef08f13c0f73
Pablo Suárez <104940521+pabloSuarezDev@users.noreply.github.com>
2023-01-15 17:50:03 +0000

    Update header.inc.php

--- views/layout/header.inc.php
+++ views/layout/header.inc.php
@@ -13,7 +13,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- STYLESHEET -->
   <link rel="stylesheet" href="<?=BASE_URL?>assets/css/styles.css">
-  <title>Tienda de Camisetas</title>
+  <title>Tienda Online</title>
 </head>
 <body>
   <div id="container">
